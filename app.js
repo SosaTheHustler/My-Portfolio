98ede367e715c2c8d67d2c63da2f112a8d349b56
Jeremy Smalls-Bushay <arcane1241@gmail.com>
2022-02-02 01:17:24 -0500

    updated navbar

--- app.js
+++ app.js
@@ -1,49 +1,38 @@
 const navSlide = () => {
-   const burger = document.querySelector('.burger');
-   const nav = document.querySelector('.menu');
-   const navLinks = document.querySelectorAll('.menu li');
-   const clicks = document.querySelector('.nav-link')
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".menu");
+  const navLinks = document.querySelectorAll(".menu li");
+  const clicks = document.querySelector(".nav-link");
 
-   burger.addEventListener('click', () =>{
-       //Toggle Nav
+  burger.addEventListener("click", () => {
+    //Toggle Nav
 
-       nav.classList.toggle('menu-active');
+    nav.classList.toggle("menu-active");
 
-
-       
-
-       nav.addEventListener('click', ()=>{
-        nav.classList.remove('menu-active');
-        burger.classList.remove('toggle');
-        navLinks.forEach((link,index) =>{
-            link.style.animation = ''
-        });
-
+    nav.addEventListener("click", () => {
+      nav.classList.remove("menu-active");
+      burger.classList.remove("toggle");
+      navLinks.forEach((link, index) => {
+        link.style.animation = "";
+      });
     });
 
-       //Animate Links
-       
-       navLinks.forEach((link,index) => {
-           if (link.style.animation) {
-               link.style.animation = '';
-           } else {
-               link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`; 
-           }
+    //Animate Links
 
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
     });
-       //Burger Animation
-       burger.classList.toggle('toggle');
-       
-
+    //Burger Animation
+    burger.classList.toggle("toggle");
 
     //Close Nav on click.
+  });
+};
 
-   });       
-
-
-}
-
-
-navSlide();        
-
-
+navSlide();
